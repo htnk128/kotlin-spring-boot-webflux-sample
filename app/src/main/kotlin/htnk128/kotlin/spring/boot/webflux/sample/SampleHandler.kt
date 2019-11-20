d3eed0fe6b6c075bcc3a07f1ae5eb872e3e14d33
hiroakitanaka128 <tanaka_hiroaki@cyberagent.co.jp>
2019-11-21 00:07:29 +0900

    [UPDATE]MediaType APPLICATION_JSON_UTF8_VALUE -> APPLICATION_JSON_VALUE

--- app/src/main/kotlin/htnk128/kotlin/spring/boot/webflux/sample/SampleHandler.kt
+++ app/src/main/kotlin/htnk128/kotlin/spring/boot/webflux/sample/SampleHandler.kt
@@ -1,6 +1,6 @@
 package htnk128.kotlin.spring.boot.webflux.sample
 
-import org.springframework.http.MediaType.APPLICATION_JSON_UTF8
+import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
@@ -13,7 +13,7 @@ class SampleHandler {
 
     fun message(req: ServerRequest): Mono<ServerResponse> {
         return ok()
-            .contentType(APPLICATION_JSON_UTF8)
+            .contentType(APPLICATION_JSON)
             .body(Flux.just("{\"message\":\"hello\"}"), String::class.java)
     }
 }
--- app/src/main/kotlin/htnk128/kotlin/spring/boot/webflux/sample/SampleRouter.kt
+++ app/src/main/kotlin/htnk128/kotlin/spring/boot/webflux/sample/SampleRouter.kt
@@ -1,7 +1,7 @@
 package htnk128.kotlin.spring.boot.webflux.sample
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.MediaType.APPLICATION_JSON_UTF8
+import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
@@ -10,7 +10,7 @@ import org.springframework.web.reactive.function.server.router
 class SampleRouter(private val sampleHandler: SampleHandler) {
 
     fun routes(): RouterFunction<ServerResponse> = router {
-        accept(APPLICATION_JSON_UTF8).nest {
+        accept(APPLICATION_JSON).nest {
             GET("/", sampleHandler::message)
         }
     }
